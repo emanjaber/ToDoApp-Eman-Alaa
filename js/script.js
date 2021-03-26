f0330e7daa2d968e51120ac4ad5bc44db1a0ed90
alaa521 <alaaahmedamer521@gmail.com>
2021-03-26 15:21:08 +0200

    add and save Relates #3

--- js/script.js
+++ js/script.js
@@ -1,21 +1,76 @@
 let todos = [];
 const todoInput = document.getElementsByClassName('Basic');
 const insertInput = document.getElementById('insert');
+let listContainer = document.querySelector("myToDoList")
 
-insertInput.addEventListener('click', function(element) {
-  element.preventDefault();
-  addTodo(todoInput.value); 
-});
+// insertInput.addEventListener('click', function(element) {
+//   addTodo(todoInput.value); 
+// });
+insertInput.addEventListener("click" , addTodo);
  
-function addTodo(item) {
-    if (item !== '') {
-      const todo = {id: Date.now(),name: item, completed: false };
-      todos.push(todo);
-      addToLocalStorage(todos);
-      todoInput.value = '';  //  clear the input 
-    }
-  }
-// ******************************************
-
+// function addTodo(item) {
+//      let add1= document.createElement("div")
+//      let str= document.createTextNode(item)
+//      add1.appendChild(str)
+//       let add2= document.querySelector(".content")
+//       add2.appendChild(add1)
+//       todoInput.value = '';
+//     }
   
+function addTodo(item){
+
+  if (todoInput.value !== "") {
+  let myStore = JSON.parse(localStorage.getItem("myTasks"));
+      if(myStore !== null){
+          myStore.push(todoInput.value);
+          localStorage.setItem("myTasks", JSON.stringify(myStore))
+            /* create my task  */
+              creatMyTask(todoInput.value);
+              todoInput.value = null;
+      }else{
+
+          todos.push(todoInput.value);
+          localStorage.setItem("myTasks", JSON.stringify(todos))
+  
+          /* create my task  */
+          creatMyTask(todoInput.value);
+          todoInput.value = null;
+  
+      }
+
+  } else {
+      alert("Please enter our task")
+  }
+}
+
+function creatMyTask (currentValue){
+
+  var task = document.createElement("li");
+  task.textContent = currentValue;
+  myToDoList.appendChild(task);
+  currentValue = null;
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  task.appendChild(span);
+  // span.addEventListener("click", deleteTask)
+
+}
+
+/* onload function */
+window.onload = function(){
+
+  let storedData = JSON.parse(localStorage.getItem("myTasks"))
+
+  if(storedData !== null){
+      for(let i=0; i<storedData.length; i++){
+
+          creatMyTask(storedData[i])
+      }
+  }
+
+}
+
+
   
